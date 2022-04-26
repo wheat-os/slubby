72ac95e2310daa3d0f4c11f66c6f7b2fb17eee0c
bandl <1658002533@qq.com>
2022-04-26 20:11:19 +0800

    test(pkg-bitset): add bitset test

---
+++ scheduler/buffer/queue_test.go
@@ -0,0 +1,38 @@
+package buffer
+
+import (
+	"testing"
+
+	"gitee.com/wheat-os/slubby/stream"
+	"github.com/stretchr/testify/require"
+)
+
+func TestQueue_Put(t *testing.T) {
+	queue := ShortQueue()
+	req, err := stream.Request(nil, "www.baidu.com", nil)
+	require.NoError(t, err)
+	queue.Put(req)
+
+	require.Equal(t, queue.Size(), 1)
+
+	nReq, _ := queue.Get()
+
+	require.Equal(t, queue.Size(), 0)
+
+	require.Equal(t, nReq, req)
+
+	// 并发测试
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			queue.Put(req)
+		}()
+	}
+
+	for i := 0; i < 10; i++ {
+		queue.Get()
+	}
+
+	require.Equal(t, queue.Size(), 0)
+
+}
