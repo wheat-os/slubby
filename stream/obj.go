9afc2b7b7c7e1f95d2b5076e31f9556f6d51e480
bandl <1658002533@qq.com>
2022-06-04 22:26:34 +0800

    feat(stream): add stream list create func

--- stream/obj.go
+++ stream/obj.go
@@ -20,9 +20,63 @@ func (s *StreamList) Iterator() func() Stream {
 	}
 
 }
+
 func StreamLists(self Stream, streams ...Stream) Stream {
 	return &StreamList{
 		Stream:  self,
 		streams: streams,
 	}
+}
+
+func StreamListRangeInt(self Stream, only func(i int) (Stream, error), start, end int) (Stream, error) {
+	lists := make([]Stream, 0, end-start+1)
+	for i := start; i <= end; i++ {
+		stream, err := only(i)
+		if err != nil {
+			return nil, err
+		}
+
+		lists = append(lists, stream)
+	}
+
+	return &StreamList{
+		Stream:  self,
+		streams: lists,
+	}, nil
+}
+
+func StreamListRangeString(self Stream, only func(value string) (Stream, error), s []string) (Stream, error) {
+	lists := make([]Stream, 0, len(s))
+
+	for _, val := range s {
+		stm, err := only(val)
+		if err != nil {
+			return nil, err
+		}
+
+		lists = append(lists, stm)
+	}
+
+	return &StreamList{
+		Stream:  self,
+		streams: lists,
+	}, nil
+}
+
+func StreamListRangeFloat(self Stream, only func(value float64) (Stream, error), f []float64) (Stream, error) {
+	lists := make([]Stream, 0, len(f))
+
+	for _, val := range f {
+		stm, err := only(val)
+		if err != nil {
+			return nil, err
+		}
+
+		lists = append(lists, stm)
+	}
+
+	return &StreamList{
+		Stream:  self,
+		streams: lists,
+	}, nil
 }
