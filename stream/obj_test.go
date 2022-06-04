a90df7d7b57555bba84e07751cf5c2715fcd4a4a
bandl <1658002533@qq.com>
2022-06-04 22:26:49 +0800

    test(stream): test straeam list obj

--- stream/obj_test.go
+++ stream/obj_test.go
@@ -30,3 +30,46 @@ func TestStreamLists(t *testing.T) {
 	require.Nil(t, nReq)
 
 }
+
+func TestStreamListRangeInt(t *testing.T) {
+	sts, err := StreamListRangeInt(nil, func(i int) (Stream, error) {
+		return Request(nil, "http://www.test/com", nil)
+	}, 10, 20)
+
+	require.NoError(t, err)
+
+	require.Equal(t, len(sts.(*StreamList).streams), 11)
+}
+
+func TestStreamListRangeString(t *testing.T) {
+	sts, err := StreamListRangeString(nil, func(i string) (Stream, error) {
+		return Request(nil, "http://www.test/com", nil)
+	}, []string{"1", "2", "3"})
+
+	require.NoError(t, err)
+
+	require.Equal(t, len(sts.(*StreamList).streams), 3)
+}
+
+func TestStreamListRangeFloat(t *testing.T) {
+	testFloat := []float64{1.1, 1.2, 1.3, 1.333}
+	sts, err := StreamListRangeFloat(nil, func(value float64) (Stream, error) {
+		req, err := Request(nil, "http://www.baidu.com", nil)
+		if err != nil {
+			return nil, err
+		}
+
+		req.Meta["val"] = value
+		return req, nil
+	}, testFloat)
+
+	require.NoError(t, err)
+
+	iter := sts.(*StreamList).Iterator()
+
+	for _, v := range testFloat {
+		it := iter()
+		value := it.(*HttpRequest).Meta["val"]
+		require.Equal(t, v, value)
+	}
+}
