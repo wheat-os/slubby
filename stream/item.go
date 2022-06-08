21fabb24cf4ae9496f7588bfc27b1e3e92516169
bandl <1658002533@qq.com>
2022-06-08 21:27:56 +0800

    feat(item): add basic filed name item

--- stream/item.go
+++ stream/item.go
@@ -3,16 +3,29 @@ package stream
 type Item interface {
 	Stream
 	Filed()
+	IName() string
 }
 
 type item struct {
 	Stream
+	name string
 }
 
 func (i *item) Filed() {}
 
+func (i *item) IName() string {
+	return i.name
+}
+
 func BasicItem(self Stream) Item {
 	return &item{
 		Stream: self,
+	}
+}
+
+func BasicItemBandName(self Stream, s string) Item {
+	return &item{
+		Stream: self,
+		name:   s,
 	}
 }
